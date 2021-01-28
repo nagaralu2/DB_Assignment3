bd00d3eb4897ab9bda0396ff0f0c92a70417b9cc
Ramesh Nagaralu <RNagaralu@milestraining.lab>
2021-01-28 00:29:46 -0500

    run assignment2 first, all queries complete to my understanding

--- TavernsQueryScript.sql
+++ TavernsQueryScript.sql
@@ -37,32 +37,47 @@ SELECT ')'  AS queryPieces
 should be a way to track Room Stays which will contain a sale, guest, room, the date it was stayed in and the rate
 */
 --rooms table
-DROP TABLE IF EXISTS [rooms];
+DROP TABLE IF EXISTS [roomStatus];
 
-CREATE TABLE [rooms] (
+CREATE TABLE [roomStatus] (
 ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-roomStatus VARCHAR(250) NOT NULL,
-tavernId INT NOT NULL FOREIGN KEY REFERENCES taverns(ID)    
+roomStatus VARCHAR(250) NOT NULL,  
 );
 
-INSERT INTO [rooms] (roomStatus, tavernId)
-VALUES ('available', 1), ('not available', 2), ('booked', 3);
+INSERT INTO [roomStatus] (roomStatus)
+VALUES ('available'), ('not available'), ('booked');
 
 --roomStays table
 DROP TABLE IF EXISTS [roomStays];
 
 CREATE TABLE [roomStays] (
 ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-sale VARCHAR(250) NOT NULL,
-guestName VARCHAR(250) NOT NULL,
+tavernId INT NOT NULL FOREIGN KEY REFERENCES taverns(ID), 
 room VARCHAR(250) NOT NULL,
+guestName VARCHAR(250) NOT NULL,
 dateIn DATE NOT NULL,
-rate MONEY NOT NULL
+rate MONEY NOT NULL,
+roomStatusId INT NOT NULL FOREIGN KEY REFERENCES roomStatus(ID) 
 );
 
---INSERT INTO [roomStays] (sale, guestName, room, dateIn, rate)
---VALUES 
+INSERT INTO [roomStays] (tavernId, room, guestName, dateIn, rate, roomStatusId)
+VALUES (1, 'single 1', 'Agnes Doyle', '01/01/2021', $70, 1),
+(1, 'single 2', '', '', $90, 2),
+(2, 'Suite A', 'Patti Flores', '01/05/2021', $150, 1),
+(3, 'Suite B', 'Marsha Black', '01/06/2021', $170, 1),
+(4, 'Villa', 'Albert Vega', '01/10/2021', $300, 1); 
 
+--roomSales table
+DROP TABLE IF EXISTS [roomSales];
+
+CREATE TABLE [roomSales] (
+ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+roomId INT NOT NULL FOREIGN KEY REFERENCES roomStays(ID),
+rateSold MONEY NOT NULL
+)
+
+INSERT INTO [roomSales] (roomId, rateSold)
+VALUES (1, $80), (2, $0), (3, $200), (4, $220), (5, $400)
 
 /*
 2. Write a query that returns guests with a birthday before 2000.
@@ -75,20 +90,25 @@ WHERE DATEPART(YEAR, birthday) < 2000
 /*
 3. Write a query to return rooms that cost more than 100 gold a night
 */
+SELECT taverns.tavernName AS tavern, roomStays.room, roomStays.rate FROM [roomStays]
+JOIN [taverns] ON roomStays.tavernId = taverns.ID
+WHERE rate > 100
 
 /*
 4.Write a query that returns UNIQUE guest names.
 */
 --inserting another row for 'Agnes Doyle' to test UNIQUE names 
-INSERT INTO [guests] (tavernId, guestName, notes, birthday, cakeday, guestStatusId, classId)
-VALUES (5, 'Agnes Doyle', 'likes oshizushi', '01/01/1980', '01/01/2021', 3, 5 )
+INSERT INTO [guests] (tavernId, guestName, notes, birthday, cakeday, guestStatusId)
+VALUES (5, 'Agnes Doyle', 'likes oshizushi', '01/01/1980', '01/01/2021', 3)
 SELECT DISTINCT guestName FROM [guests]
+
 /*
 5. Write a query that returns all guests ordered by name (ascending) Use ASC or DESC after your ORDER BY [col]
 */
 SELECT guestName FROM [guests]
 --ORDER BY guestName ASC
 ORDER BY guestName DESC
+
 /*
 6. Write a query that returns the top 10 highest price sales
 */
@@ -106,10 +126,21 @@ UNION ALL
 SELECT * FROM [guestStatus]
 UNION ALL
 SELECT * FROM [class]
+
 /*
 8. Write a query that returns Guest Classes with Levels and Generate a new column with a label for their level 
 grouping (lvl 1-10, 10-20, etc)
 */
+SELECT guests.guestName as Guest, class.className as Class, levels.level as Level, 
+    CASE
+		when level >0  and level <= 10 THEN 'Lvl 1-10'
+		when level >10 and level <= 20 THEN 'Lvl 11-20'
+		when level >20 and level <= 30 THEN 'Lvl 21-30'
+		when level >30 and level <= 40 THEN 'Lvl 31-40'
+	END as 'Level Group'
+FROM [levels]
+JOIN [guests] ON levels.guestId = guests.ID
+JOIN [class] ON levels.classId = class.ID
 
 /*
 9. Write a series of INSERT commands that will insert the statuses of one table into another of your choosing using 
@@ -117,7 +148,5 @@ SELECT statements (See our lab in class - The INSERT commands should be generate
 match or make sense! :)
 * Remember, INSERT Commands look like: INSERT INTO Table1 (column1, column2) VALUES (column1, column2) 
 */
-
-
-
---1
+INSERT INTO [servicesStatus] (status)
+SELECT status FROM [guestStatus]
